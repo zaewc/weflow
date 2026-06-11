f2c83ff7b9527406e3df92b5a3443cc4147c55cf
재욱 <haensol976520@gmail.com>
2026-06-11 09:46:58 +0900

    perf: preconnect CDN and load dynamic-subset Pretendard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,13 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         <link
+          rel="preconnect"
+          href="https://cdn.jsdelivr.net"
+          crossOrigin="anonymous"
+        />
+        <link
           rel="stylesheet"
-          as="style"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css"
         />
       </head>
       <body>
